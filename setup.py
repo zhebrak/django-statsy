2267c2c09f2d65c63382595f38052589353b27cc
Alexander Zhebrak <fata2ex@gmail.com>
2015-02-13 19:34:53 +0300

    pypi installation now works just fine

--- setup.py
+++ setup.py
@@ -2,8 +2,13 @@
 
 from distutils.core import setup
 
-__version__ = '0.1dev'
 
+__version__ = '0.0.4dev'
+
+install_requires = [
+    'Django==1.7.4',
+    'jsonfield==1.0.2'
+]
 
 setup(
     name='django-statsy',
@@ -14,6 +19,10 @@ setup(
     author_email='fata2ex@gmail.com',
     license='MIT',
     url='https://github.com/fata1ex/django-statsy',
+    download_url='https://pypi.python.org/pypi/django-statsy',
     keywords=['django', 'statistics', 'analytics'],
+    install_requires=install_requires,
+    zip_safe=False,
+    include_package_data=True,
     classifiers=[],
 )
