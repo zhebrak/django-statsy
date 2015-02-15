b2f5c65cd623d01f00064c33c563ae6d4e4f5ec3
Alexander Zhebrak <fata2ex@gmail.com>
2015-02-15 16:05:34 +0300

    test unicode str removed

--- tests/settings.py
+++ tests/settings.py
@@ -5,13 +5,9 @@ test_group = 'test_group'
 test_event = 'test_event'
 
 test_value_str = 'test_value'
-test_value_unicode = u'test_value'
 test_value_int = 123
 test_value_float = 123.0
 
 test_label = 'test_label'
 
-test_value_list = [
-    test_value_str, test_value_unicode,
-    test_value_int, test_value_float
-]
+test_value_list = [test_value_str, test_value_int, test_value_float]
