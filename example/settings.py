4462a14c7a51c32deaf014cfd04013a51320f261
Alexander Zhebrak <fata2ex@gmail.com>
2016-05-16 17:33:07 +0300

    celery is back to test en

--- example/settings.py
+++ example/settings.py
@@ -21,7 +21,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    # 'celery',
+    'celery',
 
     'statsy',
     'tests',
--- example/tasks.py
+++ example/tasks.py
@@ -2,15 +2,12 @@
 
 import statsy
 
-try:
-    from example.celery_app import app
+from example.celery_app import app
 
 
-    @app.task
-    def sample_callback_task(_):
-        statsy.send(
-            extra='I\'m the task callback'
-        )
+@app.task
+def sample_callback_task(_):
+    statsy.send(
+        extra='I\'m the task callback'
+    )
 
-except ImportError:
-    pass
